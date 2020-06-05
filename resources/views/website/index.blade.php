f5dc256492feb39c2af6b6c7f91e4e5268716edd
João G. Silva <joogabrielsntn66@gmail.com>
2020-06-05 17:36:27 -0400

    Página inicial quase concluída --falta responsividade

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -1,52 +1,193 @@
 @extends('website.layouts.default')
 
 @section('content')
-    @include('website.partials.carousel')
-    <section class="filtro-carros bg-secondary py-5">
-        <div class="container">
-            <form action="{{ route('root') }}">
-                <div class="row justify-content-center align-items-center">
-                    <div class="col-md-auto">
-                        <div class="form-group">
-                            <select name="estados" id="estados_select">
-                                <option disabled selected>Estado</option>
-                                <option value="MT">Mato Grosso</option>
-                            </select>
+    <main>
+        @include('website.partials.carousel')
+        <section class="filtro-carros bg-secondary py-5">
+            <div class="container">
+                <form action="{{ route('root') }}">
+                    <div class="row justify-content-center align-items-center">
+                        <div class="col-md-auto">
+                            <div class="form-group">
+                                <select name="estados" id="estados_select">
+                                    <option disabled selected>Estado</option>
+                                    <option value="MT">Mato Grosso</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-auto">
+                            <div class="form-group">
+                                <select name="estados" id="estados_select">
+                                    <option disabled selected>Estado</option>
+                                    <option value="MT">Mato Grosso</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-auto">
+                            <div class="form-group">
+                                <select name="estados" id="estados_select">
+                                    <option disabled selected>Estado</option>
+                                    <option value="MT">Mato Grosso</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-auto">
+                            <div class="form-group">
+                                <select name="estados" id="estados_select">
+                                    <option disabled selected>Estado</option>
+                                    <option value="MT">Mato Grosso</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-auto">
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-danger">
+                                    Encontrar carro
+                                </button>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-auto">
-                        <div class="form-group">
-                            <select name="estados" id="estados_select">
-                                <option disabled selected>Estado</option>
-                                <option value="MT">Mato Grosso</option>
-                            </select>
+                </form>
+            </div>
+        </section>
+        <section class="card-carros">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="card shadow">
+                            <div class="card-body">
+                                <div class="row justify-content-center align-items-center">
+                                    {{-- Titulo --}}
+                                    <div class="col-md-auto">
+                                        <h5 class="card-title"><strong>Lorem ipsum</strong></h5>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    {{-- Imagem --}}
+                                    <div class="col-md-auto">
+                                        <img class="card-img-top h-100 lazy img-fluid" src="{{ asset('resources/images/card-car.jpg') }}" alt="">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    {{-- Dados do Carro --}}
+                                    <div class="col">
+                                        <div class="row justify-content-center align-items-center mt-3">
+                                            {{-- Preço --}}
+                                            <div class="col-md-auto">
+                                                <p><strong>R$ XX.XXX,XX</strong></p>
+                                            </div>
+                                        </div>
+                                        <p class="card-text mb-2"><strong>Ano:</strong>20XX</p>
+                                        <p class="card-text mb-2"><strong>Local:</strong>Cuiabá-MT</p>
+                                        <p class="card-text mb-4"><strong>Quilometragem:</strong>XXXX km</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                {{-- Ir para detalhes --}}
+                                <div class="row justify-content-center align-items-center">
+                                    <div class="col-md-auto">
+                                        <a href="{{ route('root') }}" class="btn btn-link">
+                                            Estou interessado
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-auto">
-                        <div class="form-group">
-                            <select name="estados" id="estados_select">
-                                <option disabled selected>Estado</option>
-                                <option value="MT">Mato Grosso</option>
-                            </select>
+                    <div class="col">
+                        <div class="card shadow">
+                            <div class="card-body">
+                                <div class="row justify-content-center align-items-center">
+                                    {{-- Titulo --}}
+                                    <div class="col-md-auto">
+                                        <h5 class="card-title"><strong>Lorem ipsum</strong></h5>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    {{-- Imagem --}}
+                                    <div class="col-md-auto">
+                                        <img class="card-img-top h-100 lazy img-fluid" src="{{ asset('resources/images/card-car.jpg') }}" alt="">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    {{-- Dados do Carro --}}
+                                    <div class="col">
+                                        <div class="row justify-content-center align-items-center mt-3">
+                                            {{-- Preço --}}
+                                            <div class="col-md-auto">
+                                                <p><strong>R$ XX.XXX,XX</strong></p>
+                                            </div>
+                                        </div>
+                                        <p class="card-text mb-2"><strong>Ano:</strong>20XX</p>
+                                        <p class="card-text mb-2"><strong>Local:</strong>Cuiabá-MT</p>
+                                        <p class="card-text mb-4"><strong>Quilometragem:</strong>XXXX km</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                {{-- Ir para detalhes --}}
+                                <div class="row justify-content-center align-items-center">
+                                    <div class="col-md-auto">
+                                        <a href="{{ route('root') }}" class="btn btn-link">
+                                            Estou interessado
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-auto">
-                        <div class="form-group">
-                            <select name="estados" id="estados_select">
-                                <option disabled selected>Estado</option>
-                                <option value="MT">Mato Grosso</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-md-auto">
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-danger">
-                                Encontrar carro
-                            </button>
+                    <div class="col">
+                        <div class="card shadow">
+                            <div class="card-body">
+                                <div class="row justify-content-center align-items-center">
+                                    {{-- Titulo --}}
+                                    <div class="col-md-auto">
+                                        <h5 class="card-title"><strong>Lorem ipsum</strong></h5>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    {{-- Imagem --}}
+                                    <div class="col-md-auto">
+                                        <img class="card-img-top h-100 lazy img-fluid" src="{{ asset('resources/images/card-car.jpg') }}" alt="">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    {{-- Dados do Carro --}}
+                                    <div class="col">
+                                        <div class="row justify-content-center align-items-center mt-3">
+                                            {{-- Preço --}}
+                                            <div class="col-md-auto">
+                                                <p><strong>R$ XX.XXX,XX</strong></p>
+                                            </div>
+                                        </div>
+                                        <p class="card-text mb-2"><strong>Ano:</strong>20XX</p>
+                                        <p class="card-text mb-2"><strong>Local:</strong>Cuiabá-MT</p>
+                                        <p class="card-text mb-4"><strong>Quilometragem:</strong>XXXX km</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                {{-- Ir para detalhes --}}
+                                <div class="row justify-content-center align-items-center">
+                                    <div class="col-md-auto">
+                                        <a href="{{ route('root') }}" class="btn btn-link">
+                                            Estou interessado
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </form>
-        </div>
-    </section>
+                <div class="row justify-content-center align-items-center mt-3">
+                    <div class="col-md-auto">
+                        <button class="btn btn-danger">
+                            Ver Mais ...
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 @endsection
---
+++ resources/views/website/partials/footer.blade.php
@@ -0,0 +1,19 @@
+<footer class="footer page-footer font-small bg-primary p-5">
+    <div class="container">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-md-4">
+                <img class="lazy img-fluid" src="{{ asset('resources/images/youcar_logo.png') }}" alt="">
+            </div>
+            <div class="col-md-auto">
+                <div class="informacoes">
+                    <div class="row">
+                        <div class="col">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
