2a5a73be5ff072983eebe836c5954b8eb5d25835
João G. Silva <joogabrielsntn66@gmail.com>
2020-06-05 11:03:31 -0400

    Commit de segurança

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -2,4 +2,51 @@
 
 @section('content')
     @include('website.partials.carousel')
+    <section class="filtro-carros bg-secondary py-5">
+        <div class="container">
+            <form action="{{ route('root') }}">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-md-auto">
+                        <div class="form-group">
+                            <select name="estados" id="estados_select">
+                                <option disabled selected>Estado</option>
+                                <option value="MT">Mato Grosso</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-auto">
+                        <div class="form-group">
+                            <select name="estados" id="estados_select">
+                                <option disabled selected>Estado</option>
+                                <option value="MT">Mato Grosso</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-auto">
+                        <div class="form-group">
+                            <select name="estados" id="estados_select">
+                                <option disabled selected>Estado</option>
+                                <option value="MT">Mato Grosso</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-auto">
+                        <div class="form-group">
+                            <select name="estados" id="estados_select">
+                                <option disabled selected>Estado</option>
+                                <option value="MT">Mato Grosso</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-auto">
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-danger">
+                                Encontrar carro
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
 @endsection
