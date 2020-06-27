b370d715ae0ea8e4008d5ca50176ac9f19156e8b
João G. Silva <joogabrielsntn66@gmail.com>
2020-06-26 22:18:37 -0400

    Alterações finais na Página Inicial

--- resources/views/website/partials/footer.blade.php
+++ resources/views/website/partials/footer.blade.php
@@ -4,12 +4,12 @@
             <div class="col-md-4">
                 <img class="lazy img-fluid" src="{{ asset('resources/images/youcar_logo.png') }}" alt="">
             </div>
-            <div class="col-md-auto">
+            <div class="col-md-4">
                 <div class="informacoes">
                     <div class="row">
-                        <div class="col">
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                        <div class="col-md-auto">
+                            <p><strong>Endereço:</strong> Av. Dom Orlando Chaves, 548 - Cristo Rei, Várzea Grande - MT, 78115-097</p>
+                            <p><strong>Telefones:</strong> Josy Anne - (65) 98122-9088 / Ronie - (65) 98114-6433</p>
                         </div>
                     </div>
                 </div>
